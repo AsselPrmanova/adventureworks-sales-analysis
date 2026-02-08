6becfb085c3036a149a320a43e72edbfe6faf3df
AsselPrmanova <assel.prmanova@gmail.com>
2026-02-08 17:52:51 +0500

    Create sales.analysis.sql

---
+++ sql/sales.analysis.sql
@@ -0,0 +1,35 @@
+--Sales Analysis - Sales by month, quarter, year
+
+--Total Sales by Year
+
+SELECT EXTRACT(YEAR FROM o.order_date) AS year, 
+		COUNT(DISTINCT o.order_id) AS total_orders,
+		SUM(od.unit_price * od.quantity) AS total_sales
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+GROUP BY EXTRACT(YEAR FROM o.order_date)
+ORDER BY year;
+
+--Total Sales by Month
+SELECT EXTRACT(YEAR FROM o.order_date) AS year, 
+		TO_CHAR (o.order_date, 'Month') AS month,
+		COUNT(DISTINCT o.order_id) AS total_orders, 
+		SUM(od.unit_price * od.quantity) AS total_sales
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+GROUP BY EXTRACT(YEAR FROM o.order_date), 
+		TO_CHAR (o.order_date, 'Month'),
+		EXTRACT (MONTH FROM o.order_date)
+ORDER BY year asc,
+		EXTRACT (MONTH FROM o.order_date);
+
+-- Total Sales by Quarter
+SELECT EXTRACT(YEAR FROM o.order_date) AS year, 
+		EXTRACT(QUARTER FROM o.order_date) AS Quarter, 
+		COUNT(DISTINCT o.order_id) AS total_orders,
+		SUM(od.unit_price * od.quantity) AS total_sales
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+GROUP BY EXTRACT(YEAR FROM o.order_date),
+		EXTRACT(QUARTER FROM o.order_date) 
+ORDER BY year asc, Quarter asc;
