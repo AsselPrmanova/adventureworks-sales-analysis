ac20c84661add81f95af8b54221dca970625805f
Assel Prmanova <assel.prmanova@gmail.com>
2026-02-20 17:08:49 +0500

    Added top-5 countries sales query for portfolio

---
+++ sql/top_5_countries.sql
@@ -0,0 +1,16 @@
+--Identified top-5 countries by total sales using aggregated order data.
+
+SELECT
+    o.ship_country,
+    ROUND(SUM(sub.total_sales)::numeric, 2) AS total_sales
+FROM orders o
+JOIN (
+    SELECT
+        order_id,
+        SUM(unit_price * quantity) AS total_sales
+    FROM order_details
+    GROUP BY order_id
+) sub ON o.order_id = sub.order_id
+GROUP BY o.ship_country
+ORDER BY total_sales DESC
+LIMIT 5;
